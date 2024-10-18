1c1d6ef91c6f141d729c3dd1a8db307b3086fa1d
Newix200 <newix.code@gmail.com>
2024-10-18 15:04:55 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,8 @@
+'''Author:Aiwin Soji
+Date:18-10-2024
+description: program that asks the user to input their name and age. After receiving the input,
+display a message that includes the user's name and age.
+Hello, John! You are 25 years old.'''
+n=input("Enter your name ")
+a=int(input("Enter your age "))
+print("Hello,",n,"! You are",a,"years old")
