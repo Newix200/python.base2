081a179dc56b72883504d0210f5a9a80b8d68fe0
Newix200 <newix.code@gmail.com>
2024-10-18 15:06:29 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,9 @@
+'''Write a Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and
+then concatenate it with another string.
+Finally, display the new string.'''
+
+str1="Hello, World "
+str2="Everyone!"
+str3=str1[7:13]+str2
+print(str3)
