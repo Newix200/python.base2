8620eedf0137f431fa3c5eb8df664c90b14a4f04
Aiwin Soji <newix.code@gmail.com>
2024-10-19 16:17:39 +0530

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,8 @@
+""""Author:Aiwin Soji
+Date: 19-10-2024
+"""
+from datetime import datetime
+current_time=datetime.now()
+print(current_time)
+format_1=current_time.strftime("%Y-%m-%d %H:%M:%S")
+print(format_1)
