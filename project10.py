82516420e9839fd4c482188e7284e9b6ef9df8e0
Aiwin Soji <newix.code@gmail.com>
2024-10-25 16:28:58 +0530

    Add files via upload

---
+++ project10.py
@@ -0,0 +1,12 @@
+""" Author= Aiwin Soji
+ Date= 25-10-2024
+ Aim= Write a Python program to convert temperature values back and forth between Celsius and Fahrenheit.
+ The user should be able to input a temperature in Celsius or Fahrenheit, and the program should convert it to the other unit using the formula:
+ """
+
+temperature= int(input("Enter the temperature"))
+scale=input("Is this in (C)elsius or (F)ahrenheit ?")
+if scale=="C":
+    print("The temperature in fahrenheit is:",(9/5*temperature)+32)
+else:
+    print("The temperature in Celsius is:",(5/9*(temperature-32)))
