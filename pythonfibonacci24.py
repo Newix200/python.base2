69f9fe058e06dd1b07b6ba24201b885b2d30a854
Aiwin Soji <newix.code@gmail.com>
2024-12-06 16:30:51 +0530

    Add files via upload

--- pythonfibonacci24.py
+++ pythonfibonacci24.py
@@ -3,6 +3,7 @@ Author : Aiwin Soji
 Date : 06-12-2024
 Aim :Program to define a module to find Fibonacci Numbers and import the module to another program.
 '''
+
 def fibonacci(num):
     list1=[]
     num1=0
