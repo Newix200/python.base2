fe4e9b7a9b240ab91e6915d66eb7c7c4deadcd4f
Aiwin Soji <newix.code@gmail.com>
2024-12-06 15:49:53 +0530

    Pythonfibonacci24.py

--- pythonfibonacci24.py
+++ pythonfibonacci24.py
@@ -3,15 +3,13 @@ Author : Aiwin Soji
 Date : 06-12-2024
 Aim :Program to define a module to find Fibonacci Numbers and import the module to another program.
 '''
-def fibonacci(n):
+def fibonacci(num):
+    list1=[]
     num1=0
     num2=1
-    new_number=num2
-    count=1
-    while count<=n-1:
-        print(new_number,end=" ")
-        count+=1
-        num1, num2= num2, new_number
-        new_number =num1+num2
-    print()
-fibonacci(9)
+    for i in range (num):
+        list1.append(num1)
+        num1,num2=num2,num1+num2
+    return list1
+
+print(fibonacci(9))
